d4c2df4ef6f6c92150c2ab89febeb49960148131
BuracoZero <buracozero.oficial@gmail.com>
2023-09-11 10:40:20 -0300

    oi

--- src/screens/Cadastrar/index.tsx
+++ src/screens/Cadastrar/index.tsx
@@ -1,14 +1,63 @@
-import React from "react";
-import { View, KeyboardAvoidingView, Text } from "react-native"
+import React, { useEffect, useState } from "react";
+import { View, KeyboardAvoidingView, Text, Alert } from "react-native"
 import {styles} from "./styles"
 import { AntDesign, Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { TextInput } from "react-native-gesture-handler";
 import { colors } from "../../styles/colors";
-import { ComponentButtonInterface } from "../../components";
+import { ComponentButtonInterface, ComponentLoading } from "../../components";
 import { LoginTypes } from "../../navigations/login.navigation";
+import { MaskedTextInput } from "react-native-mask-text";
+import { IRegister } from '../../services/data/User'
+import { apiUser } from "../../services/data";
+import { AxiosError } from "axios";
+export interface IErrorApi {
+    errors: {
+        rule: string
+        field: string
+        message: string
+    }[]
+}
 
 export function Cadastrar({navigation}:LoginTypes) {
+    const [data, setData] = useState<IRegister>()
+    const [ isLoading, setIsLoading ] = useState(true)
+    async function handleRegister() {
+        try {
+            setIsLoading(true)
+            if(data?.name && data.email && data.password){
+                const response = await apiUser.register(data)
+                Alert.alert(`${response.data.name}cadastrado(a)!!!`)
+                navigation.navigate('Login')
+            } else {
+                Alert.alert("Preencha todos os campos!!!")
+            }
+        } catch (error) {
+            const err = error as AxiosError
+            const errData = err.response?.data as IErrorApi
+            let message = ""
+            if(errData) {
+                for (const iterator of errData.errors){
+                    message = `${message}${iterator.message} \n`
+                }
+            }
+            Alert.alert(message)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+    function handleChange(item: IRegister) {
+        setData({...data, ...item})
+    }
+    useEffect(()=>{
+        setTimeout(()=>{
+            setIsLoading(false)
+        },750)
+    },[])
     return (
+        <>
+        {isLoading ? (
+            <ComponentLoading/>
+        ) : (
         <View style={styles.container}>
             <KeyboardAvoidingView>
                 <Text style={styles.title}>Cadastrar</Text>
@@ -22,10 +71,15 @@ export function Cadastrar({navigation}:LoginTypes) {
                 </View>
                 <View style={styles.formRow}>
                 <AntDesign name="idcard" size={24} color="black" />
-                <TextInput
+                <MaskedTextInput
+                    mask="999.999.999-99"
+                    onChangeText={(text, rawText) => {
+                      console.log(text);
+                      console.log(rawText);
+                    }}
                     placeholder="CPF"
                     placeholderTextColor={colors.black}
-                    keyboardType="email-address"
+                    keyboardType="numeric"
                     autoCapitalize="none"
                     style={styles.input}
                     />
@@ -40,9 +94,11 @@ export function Cadastrar({navigation}:LoginTypes) {
                     style={styles.input}
                     />
                 </View>
-                <ComponentButtonInterface title="Salvar" type="secondary" onPressI={()=>console.log("cadastrar")}/>
+                <ComponentButtonInterface title="Salvar" type="secondary" onPressI={handleRegister}/>
                 <ComponentButtonInterface title="Voltar" type="primary" onPressI={()=>navigation.navigate("Login")}/>
             </KeyboardAvoidingView>
         </View>
-    )
+     )}
+     </>
+ );
 }
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,23 +1,59 @@
-import React from "react";
-import { View, Text, KeyboardAvoidingView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, KeyboardAvoidingView, Alert } from "react-native";
 import {styles} from "./styles";
 import { AntDesign, Ionicons } from '@expo/vector-icons';
 import { colors } from "../../styles/colors";
 import {TextInput} from "react-native-gesture-handler";
 import { ComponentButtonInterface } from "../../components";
 import {LoginTypes} from "../../navigations/login.navigation"
+import { MaskedTextInput } from "react-native-mask-text";
+import { AxiosError } from "axios";
+import { IAuthenticate } from "../../services/data/User";
+import { useAuth } from "../../hooks/auth";
 
 export function Login({navigation}:LoginTypes) {
+    const { signIn } = useAuth();
+    const [data, setData] = useState<IAuthenticate>();
+    const [isLoading, setIsLoading] = useState(true);
+    async function handleSigIn() {
+        try {
+            setIsLoading(true);
+            if (data?.email && data.password) {
+                await signIn(data);
+            }else{
+                Alert.alert("Preencha todos os campos!!!");
+                setIsLoading(false);
+            }
+        }catch (error) {
+            const err = error as AxiosError;
+            const message = err.response?.data as string
+            Alert.alert(message)
+            setIsLoading
+        }
+    }
+    function handleChange(item: IAuthenticate) {
+        setData({...data, ...item})
+    }
+    useEffect(()=>{
+        setTimeout(()=>{
+            setIsLoading(false)
+        },750)
+    },[])
     return (
         <View style={styles.container}>
             <KeyboardAvoidingView>
                 <Text style={styles.title}>Login</Text>
                 <View style={styles.formRow}>
                 <AntDesign name="idcard" size={24} color="black" />
-                <TextInput
+                <MaskedTextInput
+                    mask="999.999.999-99"
+                    onChangeText={(text, rawText) => {
+                      console.log(text);
+                      console.log(rawText);
+                    }}
                     placeholder="CPF"
                     placeholderTextColor={colors.black}
-                    keyboardType="email-address"
+                    keyboardType="numeric"
                     autoCapitalize="none"
                     style={styles.input}
                     />
@@ -32,9 +68,10 @@ export function Login({navigation}:LoginTypes) {
                     style={styles.input}
                     />
                 </View>
-                <ComponentButtonInterface title="Entrar" type="secondary" onPressI={()=>{navigation.navigate("Tab")}}/>
+                <ComponentButtonInterface title="Entrar" type="secondary" onPressI={handleSigIn}/>
                 <ComponentButtonInterface title="Cadastrar" type="primary" onPressI={()=>navigation.navigate("Cadastrar")}/>
             </KeyboardAvoidingView>
         </View>
     )
 }
+
